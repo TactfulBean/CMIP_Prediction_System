a2ffe53dce00e7040651a82183256e7be5b55b1b
TactfulBean <tactfulbean@gmail.com>
2022-11-09 19:20:02 +0800

    *

---
+++ src/components/Aside.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <AsideMenu></AsideMenu>
+  </div>
+</template>
+<script>
+import AsideMenu from "@/components/AsideMenu.vue";
+export default {
+  components: { AsideMenu },
+  setup() {},
+};
+</script>
+<style></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="home-container">
+    <el-container>
+      <el-header><Header></Header></el-header>
+      <el-container>
+        <el-aside width="200px"><Aside></Aside></el-aside>
+        <el-main><Main></Main></el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+<script>
+import Header from "@/components/Header.vue";
+import Aside from "@/components/Aside.vue";
+import Main from "@/components/Main.vue";
+
+export default {
+  setup() {},
+  components: {
+    Header,
+    Aside,
+    Main,
+  },
+};
+</script>
+<style>
+.el-header {
+  font-size: 20px;
+  background-color: #434394;
+}
+.el-aside {
+  width: 200px;
+  background-color: #99ccff;
+}
+.el-main {
+  width: auto;
+  position: relative;
+  overflow: hidden;
+}
+</style>
