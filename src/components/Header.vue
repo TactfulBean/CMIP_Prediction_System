9bd744805a78c110306447d1ef8bebe5f7888f48
TactfulBean <2662367665@qq.com>
2023-01-13 11:16:28 +0800

    *

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,7 @@
   <el-menu :ellipsis="false" mode="horizontal" background-color="#4c6cb3" text-color="#fff" active-text-color="#409EFF">
     <div class="title">CMIP6-极端气候预测系统</div>
     <div class="flex-grow" />
-    <el-menu-item index="1" @click="jumpPage('https://tactfulbean.eu.org/')">
+    <el-menu-item index="1" @click="jumpPage('https://tactfulbean.tk/')">
       <span class="nav-item" style="padding-left: 5px"><span class="iconfont">&#xeaf9; </span>Blog</span></el-menu-item
     >
     <el-menu-item index="2" @click="jumpPage('https://github.com/TactfulBean')"
